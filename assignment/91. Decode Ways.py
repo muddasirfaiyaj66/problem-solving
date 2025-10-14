101a6fab08af671075128014a5114e6b09aa6d5d
muddasir-faiyaj <muddasirfaiyaj66@gmail.com>
2025-10-14 23:58:52 +0600

    solved leetcode 91.Decode Ways problem

---
+++ assignment/91. Decode Ways.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def numDecodings(self, s):
+        dp = {len(s):1}
+        
+        def dfs(i):
+            if i in dp:
+                return dp[i]
+            if s[i] == "0":
+                return 0
+            
+            res= dfs(i+1)
+            if(i+1 < len(s) and (s[i] == "1" or s[i] == "2" and s[i+1] in "0123456")):
+                res += dfs(i+2)
+            dp[i]=res
+            return res
+        return dfs(0)
+    
+    
+solution =Solution()
+
+      
+case_1 ="12"
+case_2 ="226"
+case_3="06"
+
+print(solution.numDecodings(case_1))
+print(solution.numDecodings(case_2))
+print(solution.numDecodings(case_3))

b815cbe8021eed5a2962bce46a08562071b12346
muddasir-faiyaj <muddasirfaiyaj66@gmail.com>
2025-10-22 21:43:06 +0600

    solved leetcode problem no 300

---
+++ assignment/300. Longest Increasing Subsequence.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        n = len(nums)
+        dp =[1] * n
+        
+        for i in range(n):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i]=max(dp[i],dp[j]+1)
+        return max(dp)
+    
+    
+sol=Solution()
+
+nums = [10,9,2,5,3,7,101,18]
+print(sol.lengthOfLIS(nums))
+        
